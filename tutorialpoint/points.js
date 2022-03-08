18600d1650ce7694ec01ed574d2d4a7aa100f6e8
stephanusst <stephanusst@gmail.com>
2022-03-08 09:24:05 +0700

    Update points.js

--- tutorialpoint/points.js
+++ tutorialpoint/points.js
@@ -6,7 +6,7 @@
             'attribute vec3 coordinates;' +
 
             'void main(void) {' +
-               ' gl_Position = vec4(coordinates, 1.0);' +
+               'gl_Position = vec4(coordinates, 1.0);' +
                'gl_PointSize = 10.0;'+
             '}';
 
