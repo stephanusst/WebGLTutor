ba43954bbeef19e898d7bef93f79afac1be4102f
stephanusst <stephanusst@gmail.com>
2022-03-08 11:44:32 +0700

    Update points.js

--- tutorialpoint/points.js
+++ tutorialpoint/points.js
@@ -22,7 +22,7 @@
          // fragment shader source code
          var fragCode =
             'void main(void) {' +
-               ' gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);' +
+               ' gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);' +
             '}';
 
          // Create fragment shader object
